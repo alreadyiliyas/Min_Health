57ec9d5dc6d61d377e44b51502c15202e84e19e0
Iliyas Ukenov <iliyasuken@gmail.com>
2023-11-25 12:42:41 +0600

    Добавьте файлы проекта.

---
+++ MinHealth.DAL/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinHealth.DAL.Entities
+{
+	//1.	Пациент (Фамилия, Имя, Отчество, ИИН)
+	public class Client : User
+	{
+		public string IIN { get; set; }
+		public string PhoneNumber { get; set; }
+		public int Sex { get; set; }
+		public string Address { get; set; }	
+		public DateTime DateofBirthDay { get; set; }
+	}
+}
---
+++ MinHealth.DAL/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinHealth.DAL.Entities
+{
+	//1.	Пациент (Фамилия, Имя, Отчество, ИИН)
+	public abstract class User
+	{
+		public int Id { get; set; }
+		public string FName { get; set; }
+		public string SName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime CreationDate { get; set; }
+	}
+}
